c0be02dffb37c726b5a56f5a6466e64613bef9ef
abhisheks-gh <abhishek8598kumar@gmail.com>
2025-11-03 22:55:42 +0530

    Update comment in CallingStaticMethodOnNullObjectReference.java

--- src/main/java/com/oops/CallingStaticMethodOnNullObjectReference.java
+++ src/main/java/com/oops/CallingStaticMethodOnNullObjectReference.java
@@ -1,7 +1,11 @@
 package com.oops;
 
-// Invoking static method on null object reference does not return a NullPointerException
-// as static members belongs to class.
+/**
+ * Invoking static method on null object reference does not return a NullPointerException
+ * as static members belongs to class.
+ * In the same scenario, if an instance method was invoked - it would have resulted in
+ * a NullPointerException.
+ */
 public class CallingStaticMethodOnNullObjectReference {
     static String str;
 
