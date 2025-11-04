7f8744cea0a31d6cca773d4379be5ed5154930b6
abhisheks-gh <abhishek8598kumar@gmail.com>
2025-11-04 23:55:40 +0530

    Add using throws keyword with run() and main() method

---
+++ src/main/java/com/core_java/ThrowsWithMainAndRunMethod.java
@@ -0,0 +1,8 @@
+package com.core_java;
+
+public class ThrowsWithMainAndRunMethod {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Try to do something, if something fails - catch will handle the exception.");
+    }
+}
