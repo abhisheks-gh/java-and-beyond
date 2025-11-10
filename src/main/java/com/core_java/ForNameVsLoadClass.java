78fb2fa38a5e6a718bef5d45d729dd8282944b97
abhisheks-gh <abhishek8598kumar@gmail.com>
2025-11-10 18:47:04 +0530

    Add ForName vs LoadClass class

---
+++ src/main/java/com/core_java/ForNameVsLoadClass.java
@@ -0,0 +1,11 @@
+package com.core_java;
+
+// difference between Class.forName() vs ClassLoader.loadClass()
+public class ForNameVsLoadClass {
+
+    static {
+        System.out.println("This is executing from static block");
+    }
+
+
+}
