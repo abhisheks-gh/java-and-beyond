c3c9f73f3aa27b5880a64a99769f5507b83b2c85
abhisheks-gh <abhishek8598kumar@gmail.com>
2025-11-18 23:57:52 +0530

    Create Dynamic Invocation Handler

---
+++ src/main/java/com/core_java/DynamicInvocationHandler.java
@@ -0,0 +1,9 @@
+package com.core_java;
+
+import java.lang.reflect.InvocationHandler;
+import java.util.logging.Logger;
+
+public class DynamicInvocationHandler implements InvocationHandler {
+
+
+}
