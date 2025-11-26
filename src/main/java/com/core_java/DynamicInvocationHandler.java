b1cd91a004398cfd1afd022ef895796cbb096333
abhisheks-gh <abhishek8598kumar@gmail.com>
2025-11-26 14:44:21 +0530

    Add proxies and dynamic proxies in Java

--- src/main/java/com/core_java/DynamicInvocationHandler.java
+++
@@ -1,9 +0,0 @@
-package com.core_java;
-
-import java.lang.reflect.InvocationHandler;
-import java.util.logging.Logger;
-
-public class DynamicInvocationHandler implements InvocationHandler {
-
-
-}
